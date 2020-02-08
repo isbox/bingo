996a84f903d8d4c6b593c3ee383117ef7a0fdecd
box <thereisbox@foxmail.com>
2020-02-09 00:20:20 +0800

    feat: 图片预览键盘支持，controlbar功能完成

--- packages/img-prev/src/index.js
+++ packages/img-prev/src/index.js
@@ -8,6 +8,8 @@ const openStatus = {
   closed: 'closed'
 };
 const PREVIEW_INDEX_NAME = 'data-privew-index';
+const WHEEL_MAX_VAL = 500; // 最大滚动值
+const MAX_SCALE = 50;
 
 export default class ImgsPreview extends PureComponent {
   constructor(props) {
@@ -15,7 +17,9 @@ export default class ImgsPreview extends PureComponent {
     this.previewDom = createRef();
     this.state = {
       index: 0,
-      previewOpen: openStatus.closed
+      previewOpen: openStatus.closed,
+      mouseWheelValue: 0,
+      imgRotate: 0,
     }
   }
 
@@ -29,6 +33,35 @@ export default class ImgsPreview extends PureComponent {
     current.removeEventListener('click', this.caputreImgsClick);
   }
 
+  // 键盘监听
+  keyboardListener = (operation) => {
+    const listener = {
+      add: 'addEventListener',
+      remove: 'removeEventListener', 
+    };
+    const result = listener[operation];
+    result && document[result]('keydown', this.keyboardEvent);
+  }
+
+  // 键盘事件
+  keyboardEvent = (e) => {
+    switch(e.keyCode) {
+      case 27: // esc
+        return this.previewClose();
+      case 38: // 上箭头
+        return this.scaleBtnClick(1)();
+      case 40: // 下箭头
+        return this.scaleBtnClick(-1)();
+      case 37: // 左箭头
+        return this.swtichPreviewingImg(-1)();
+      case 39: // 右箭头
+        return this.swtichPreviewingImg(1)();
+      case 32: // 空格键
+        return this.rotateImg();
+      default:
+    }
+  }
+      
   caputreImgs = () => {
     const { current } = this.previewDom;
     const imgs = this.imgDomsFactory(current.getElementsByTagName('img'));
@@ -38,8 +71,10 @@ export default class ImgsPreview extends PureComponent {
   caputreImgsClick = (e) => {
     const { target } = e;
     if (target.tagName.toLowerCase() === 'img') {
-      const index = target.getAttribute(PREVIEW_INDEX_NAME);
+      const index = Number(target.getAttribute(PREVIEW_INDEX_NAME));
       this.setState({ previewOpen: openStatus.open, index });
+      this.listenMouseWhell();
+      this.keyboardListener('add'); // 添加键盘监听
     }
   }
   
@@ -63,6 +98,81 @@ export default class ImgsPreview extends PureComponent {
     return imgs;
   }
 
+  imgScaleStyle = () => {
+    const { mouseWheelValue, imgRotate } = this.state;
+    const coefficient = MAX_SCALE / WHEEL_MAX_VAL;
+    const result = (mouseWheelValue * coefficient) / 100;
+    return {
+      transform: `scale(${1 + result}) rotate(${imgRotate}deg)`,
+    }
+  }
+
+  previewClose = (e) => {
+    // 仅点击触发元素才执行，避免再写阻止冒泡，当没有参数时，直接关闭
+    if (!e || e.target === e.currentTarget) {
+      const { maskClose = true } = this.props;
+      this.keyboardListener('remove'); // 删除键盘监听
+      maskClose && this.setState({ previewOpen: openStatus.closing }, () => {
+        setTimeout(() => this.setState({ previewOpen: openStatus.closed }), 300);
+      });
+    }
+  }
+
+  // 鼠标滚动监听
+  listenMouseWhell = (e) => {
+    if (e) {
+      if (this.wheelTimer) {
+        return false;
+      }
+      const { wheelDeltaY, deltaY } = e.nativeEvent 
+      this.wheelTimer = setTimeout(() => {
+        const mouseWheelValue = this.state.mouseWheelValue + (wheelDeltaY || -deltaY);
+        this.refreshWheelValue(mouseWheelValue)
+        this.wheelTimer = null;
+      }, 300);
+    }
+  }
+
+  scaleBtnClick = (scale) => {
+    return () => {
+      const { mouseWheelValue } = this.state;
+      this.refreshWheelValue(mouseWheelValue + (WHEEL_MAX_VAL / 5) * scale);
+    }
+  }
+
+  // 更新滚轮数值
+  refreshWheelValue = (wheelValue) => {
+    const symbolVal = wheelValue > 0 ? 1 : -1;
+    Math.abs(wheelValue) > WHEEL_MAX_VAL && (wheelValue = WHEEL_MAX_VAL * symbolVal);  
+    this.setState({ mouseWheelValue: wheelValue });
+  }
+
+  // 切换预览图片
+  swtichPreviewingImg = (direction) => {
+    return () => {
+      const { index, imgs } = this.state;
+      const result = index + direction;
+      if (result < imgs.length && result >= 0) {
+        this.setState({
+          index: index + direction,
+          imgRotate: 0,
+          mouseWheelValue: 0,
+        }); 
+      }
+    }
+  }
+
+  // 旋转图片
+  rotateImg = () => {
+    if (this.rotateTimer) {
+      return false;
+    }
+    this.rotateTimer = setTimeout(() => {
+      this.setState({ imgRotate: this.state.imgRotate + 90 })
+      this.rotateTimer = null;
+    }, 300);
+  }
+
   render() {
     const { previewOpen, imgs, index } = this.state;
     const { children, className } = this.props;
@@ -74,11 +184,43 @@ export default class ImgsPreview extends PureComponent {
         </div>
         {
           openStatus[previewOpen] !== openStatus.closed && (
-            <div className={classnames(styles.imgPriviewMask, styles.blackMask)}>
-              <img className={styles.targetImg} src={imgs[index].src} alt="预览"/>
-              <div className="">
-                s
+            <div
+              className={classnames(styles.imgPriviewMask, styles.blackMask, { [styles.maskClosing]: previewOpen === openStatus.closing })}
+              onClick={this.previewClose}
+            >
+              <img
+                onWheel={this.listenMouseWhell}
+                className={styles.targetImg}
+                style={this.imgScaleStyle()}
+                src={imgs[index].src}
+                alt={imgs[index].src}
+              />
+              <div className={styles.controlBar}>
+                <i
+                  className="bingo bingo-minus"
+                  onClick={this.scaleBtnClick(-1)}
+                />
+                <i
+                  className="bingo bingo-add"
+                  onClick={this.scaleBtnClick(1)}
+                />
+                <i
+                  className="bingo bingo-refresh"
+                  onClick={this.rotateImg}
+                />
+                <i
+                  className="bingo bingo-left-arrow"
+                  onClick={this.swtichPreviewingImg(-1)}
+                />
+                <i
+                  className="bingo bingo-right-arrow"
+                  onClick={this.swtichPreviewingImg(1)}
+                />
               </div>
+              <i
+                className={classnames(styles.closedIcon, 'bingo bingo-closed')}
+                onClick={this.previewClose}
+              />
             </div>
           )
         }
